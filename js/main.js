469287f009348e71ed9c68e2c519c0834b40635b
Xenogenisis <mileszhang459@gmail.com>
2020-07-12 10:46:07 -0400

    attempted to add generation and if failed terribly

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const gameData = {
-    number: new Decimal("1e-1e500"),
-    negativeExponent: true
+    number: new Decimal("1e-1000"),
+    negativeExponent: true,
+    numGen: 0,
+    numGenCost: new Decimal("1e-1000"),
+    numGenPower: new Decimal("1e-1000")
 };
 
 const number = new Vue({
@@ -11,3 +14,16 @@ const number = new Vue({
         mainNumber: gameData
     },
 });
+
+function buyNumGen() {
+    if (gameData.numGenCost.lte(gameData.number)) {
+        gameData.numGen += 1;
+        gameData.number.sub(gameData.numGenCost);
+    }
+}
+
+function numberGenFunction() {
+    gameData.number.add(gameData.numGenPower.mul(gameData.numGen));
+}
+
+const mainGameLoop = window.setInterval(numberGenFunction(), 1000);
